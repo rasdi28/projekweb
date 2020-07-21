a7afde6763a6ea42ad7a96e50719e1b00c9e0b33
unknown <rasdi.abdulrohman15120@student.unsika.ac.id>
2020-07-21 13:49:19 +0700

    penambahan

---
+++ bangunruko.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>SIGMAT PROJECT</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: NewBiz
+    Theme URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+  <?php include 'header.php';
+  ?>
+
+<section id="about">
+      <div class="container">
+        <header class="section-header">
+          <p><h3>Bangun Ruko</h3>
+          <p>Membangun rumah dengan bantuan jasa kontraktor bangun rumah memang akan sangat mempermudah Anda. Dengan bekerja sama dengan kami, Anda dapat membangun rumah dengan berbagai desain yang Anda inginkan.</p>
+        </header>
+        <h3>Harga Sederhana <b>Rp. 3.500.000 /m2</b></h3>
+        <div class="row about-container">
+          <div class="col-lg-4 content order-lg-1 order-2">
+            <div class="icon-box wow fadeInUp">
+              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+              <h4 class="title"><a href="">Spesifikasi</a></h4>
+              <p class="description">
+             &#10004;Termasuk Jasa Tukang <br>&#10004;Kloset Duduk toto
+            <br>&#10004;Hand Shower Onda/Setara<br> &#10004;Keran KM Onda/Setara
+            <br> &#10004;Pintu Kamar Mandi PVC/Alumunium
+            <br> &#10004;Keramik Lantai Kamar mandi 25×25
+            <br> &#10004;Keramik Dinding KM 25×40
+            <br> &#10004;Bata Merah
+            <br> &#10004;Semen Holcim
+            <br> &#10004;Besi Tulangan Full SNI
+            <br> &#10004;Rangka Atap Baja Ringan
+            <br> &#10004;Genteng Morando/Setara
+            <br> &#10004;Plafond Gypsum Indoboard
+            <br> &#10004;Lisplank 20cm
+            <br> &#10004;Lampu Downlight
+            <br> &#10004;Lantai Granit 60×60
+            <br> &#10004;Pintu Double Meranti/Kamper
+            <br> &#10004;Kusen & Jendela Kayu Meranti
+            <br> &#10004;Cat Interior Dulux Catylac/Setara
+            <br> &#10004;Cat Eksterior Dulux Catylac/Setara
+            </div>
+          </div>
+
+          <div class="col-lg-4 content order-lg-1 order-2">
+            <div class="icon-box wow fadeInUp">
+              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+              <h4 class="title"><a href="">Fasilitas</a></h4>
+              <p class="description">&#10004;Garansi 3 Bulan
+              <br> &#10004;Bonus Layout
+              <br> &#10004;Bonus Gambar 3D Eksterior
+              <br> &#10004;Beban Konsultasi
+              <br> &#10004;Beban Survey
+
+            </p>
+            </div>
+          </div>
+         <div class="col-lg-4 content order-lg-1 order-2">
+            <img src="img/rumah.svg" class="img-fluid" alt="">
+          </div>
+        </div>
+      
+
+      <h3>Harga Berkualitas <b>Rp. 4.500.000 /m2</b></h3>
+        <div class="row about-container">
+          <div class="col-lg-4 content order-lg-1 order-2">
+            <div class="icon-box wow fadeInUp">
+              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+              <h4 class="title"><a href="">Spesifikasi</a></h4>
+              <p class="description">
+             &#10004;Termasuk Jasa Tukang <br>&#10004;Termasuk Jasa Tukang
+                <br>&#10004;Kloset Duduk Toto/Setara
+                <br>&#10004;Hand Shower Wasser/Setara
+                <br>&#10004;Keran KM Wasser/Setara
+                <br>&#10004;Pintu KM Alumunium/UPVC
+                <br>&#10004;Keramik KM Lantai 25×25
+                <br>&#10004;Keramik KM Dinding 25×40
+                <br>&#10004;Bata Merah 
+                <br>&#10004;Semen Holcim
+                <br>&#10004;Besi Tulangan Full SNI
+                <br>&#10004;Rangka Atap Baja Ringan
+                <br>&#10004;Genteng Plat Beton Kanmuri
+                <br>&#10004;Plafond Gypsum Jayaboard
+                <br>&#10004;Lisplank 30cm
+                <br>&#10004;Lampu Downlight Philips
+                <br>&#10004;Lantai Granit 60×60 Roman/Setara
+                <br>&#10004;Pintu Double Alm/UPVC
+                <br>&#10004;Kusen & Jendela Alm/Upvc
+                <br>&#10004;Cat Interior Dulux/Setara
+                <br>&#10004;Cat Eksterior Dulux/Setara
+
+            </div>
+          </div>
+
+          <div class="col-lg-4 content order-lg-1 order-2">
+            <div class="icon-box wow fadeInUp">
+              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+              <h4 class="title"><a href="">Fasilitas</a></h4>
+              <p class="description">&#10004;Garansi 3 Bulan
+                <br>&#10004;Bonus Layout
+                <br>&#10004;Bonus Gambar 3D Eksterior 
+                <br>&#10004;Bonus Gambar IMB 
+                <br>&#10004;Free Survey 
+
+
+            </p>
+            </div>
+          </div>
+         <div class="col-lg-4 content order-lg-1 order-2">
+            <img src="img/rumah.svg" class="img-fluid" alt="">
+          </div>
+        </div>
+        
+
+      <h3>Harga Mewah <b>Rp. 5.500.000 /m2</b></h3>
+        <div class="row about-container">
+          <div class="col-lg-4 content order-lg-1 order-2">
+            <div class="icon-box wow fadeInUp">
+              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+              <h4 class="title"><a href="">Spesifikasi</a></h4>
+              <p class="description">
+            &#10004;Termasuk Jasa Tukang
+            <br>&#10004;Kloset Duduk Kohler/Setara
+            <br>&#10004;Digital Shower
+            <br>&#10004;Kran Panas Dingin Toto/Setara
+            <br>&#10004;Pintu Kamar Mandi UPVC
+            <br>&#10004;Keramik Lt KM 30×30 Roman
+            <br>&#10004;Keramik Dinding KM 30×60
+            <br>&#10004;Waterheater Solar
+            <br>&#10004;Bata Merah 
+            <br>&#10004;Semen Tiga Roda
+            <br>&#10004;Besi Tulangan Full SNI
+            <br>&#10004;Rangka Atap Baja Ringan
+            <br>&#10004;Genteng Kanmuri
+            <br>&#10004;Plafond PVC Shunda/Setara
+            <br>&#10004;Lisplank 30cm
+            <br>&#10004;Lampu Downlight Philips
+            <br>&#10004;Saklar Lampu Panasonic
+            <br>&#10004;Lantai Granit 60×60 Roman
+            <br>&#10004;Pintu Double UPVC
+            <br>&#10004;Kusen & Jendela UPVC
+            <br>&#10004;Cat Interior Jotun/Setara
+            <br>&#10004;Cat Eksterior Jotun/Setara
+
+
+            </div>
+          </div>
+
+          <div class="col-lg-4 content order-lg-1 order-2">
+            <div class="icon-box wow fadeInUp">
+              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+              <h4 class="title"><a href="">Fasilitas</a></h4>
+              <p class="description">&#10004;Garansi 3 Bulan
+                <br>&#10004;Bonus Layout
+                <br>&#10004;Bonus Gambar 3D Eksterior 
+                <br>&#10004;Bonus Gambar IMB 
+                <br>&#10004;Free Survey 
+
+
+            </p>
+            </div>
+          </div>
+         <div class="col-lg-4 content order-lg-1 order-2">
+            <img src="img/rumah.svg" class="img-fluid" alt="">
+          </div>
+        </div>
+    </section>
+
+    <?php include "footer.php";
+    ?>
+    </body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+  <header id="header" class="fixed-top">
+    <div class="container">
+
+      <div class="logo float-left">
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <h1 class="text-light"><a href="#header"><span>NewBiz</span></a></h1> -->
+        <a href="#intro" class="scrollto"><img src="img/fauzi.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="main-nav float-right d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.php">Home</a></li>
+          <li class="drop-down"><a href="">Jasa</a>
+            <ul>
+              <li><a href="bangunrumah.php">Bangun Rumah</a></li>
+              <li><a href="bangunruko.php">Bangun Ruko</a></li>
+              <li><a href="#">Desain Rumah</a></li>
+              <li><a href="#">Renovasi Rumah</a></li>
+              <li><a href="#">Meningkat Rumah</a></li>
+              <li><a href="#">Pengecetan</a></li>
+              <li><a href="#">Pembuatan DAK</a></li>
+              <li><a href="#">Atap Baja Ringan</a></li>
+            </ul>
+          </li>
+          <li><a href="#intro">Portofolio</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+        </ul>
+      </nav><!-- .main-nav -->
+      
+    </div>
+  </header>
